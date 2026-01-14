913b3b4e3b0587ce12e558c3fed2a551d74a6a23
kloworizer <esha.indra@gmail.com>
2026-01-14 10:05:24 +0700

    Add user password change functionality with corresponding templates

--- diamond_web/urls.py
+++ diamond_web/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('notifications/read/<int:pk>/', views.mark_notification_read, name='mark_notification_read'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password_form.html', success_url=reverse_lazy('user_password_change_done')), name='user_password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/change_password_done.html'), name='user_password_change_done'),
 ]
